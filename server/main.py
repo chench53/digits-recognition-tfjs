ee91b3f0b47c39da28ae4b0d273cb2e4c7d21006
chench53 <chench53.logic@gmail.com>
2019-07-16 19:21:36 +0800

    add canvas, enable cors on server

--- server/main.py
+++ server/main.py
@@ -3,14 +3,17 @@
     the target modle.json file is at http://localhost:5000/models/mnist/model.json
 """
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__,
             static_url_path='/models', 
             static_folder='models')
+
+cors = CORS(app) 
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- web/model.js
+++ web/model.js
@@ -1,7 +1,7 @@
 const width = 28;
 const height = 28;
+const MODEL_URL = 'http://localhost:5000/models/mnist/model.json' 
 
-const MODEL_URL = 'http://59.110.42.211:8887/mnist/model.json' // 
 var loadModel = (async function() {
     window.model = await tf.loadLayersModel(MODEL_URL);
     console.log('load model')
@@ -11,19 +11,18 @@ loadModel().then();
 var load_img = function(img) {
     let tensor = tf.browser.fromPixels(img)
         .resizeNearestNeighbor([width, height])
-//         .resizeBilinear([width, height])
         .mean(2)
         .expandDims()
         .toFloat()
-    return tensor.div(255.0);
+        .div(255.0)
+    return tensor;
 };
 var predict = async function(id) {
     var model = window.model;
     var canvas = document.getElementById(id);
     var example = this.load_img(canvas);
     let prediction = await model.predict(example).data();
-    // console.log(prediction)
     let results = Array.from(prediction);
-//     console.log(results);
+
     return results
 }
